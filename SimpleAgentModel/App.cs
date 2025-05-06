df1b68f559ed555470d280dc71386dd8021edc0d
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-05-06 12:35:49 +0200

    better control

--- SimpleAgentModel/App.cs
+++ SimpleAgentModel/App.cs
@@ -8,13 +8,34 @@ public class App
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             AnsiHelper.Enable();
 
-        RunModel();
+        var data = ParseArgs(args);
+
+        if (!data.MultipleRun)
+            RunModel(data.X, data.Y, data.Path, true);
     }
 
-    public static void RunModel()
+    public static (int X, int Y, string Path, bool MultipleRun, int RunsCount) ParseArgs(string[] args)
+    {
+        int x, y, runsCount = 0;
+        string path;
+        bool multiple;
+        int.TryParse(args[0], out x);
+        int.TryParse(args[1], out y);
+        path = args[2];
+        multiple = args[3].StartsWith("1") || args[3].ToLower().StartsWith("true");
+        if (multiple)
+            int.TryParse(args[4], out runsCount);
+
+        if (x <= 0 || y <= 0 || (multiple && runsCount <= 0))
+            throw new ApplicationException("Invalid parameters from console.");
+
+        return (x, y, path, multiple, runsCount);
+    }
+
+    public static void RunModel(int x, int y, string path, bool shouldDraw)
     {
         bool wait = true;
-        var model = new Model(50, 15, "./Agents/Forest.agent");
+        var model = new Model(x, y, path);
 
         model.Randomize();
         model.Draw();
@@ -22,7 +43,7 @@ public class App
 
         while (!model.ShouldEnd)
         {
-            if (wait)
+            if (wait && shouldDraw)
             {
                 inp = Console.ReadLine();
                 if (inp is not null && inp.StartsWith("c"))
@@ -30,7 +51,8 @@ public class App
             }
 
             model.Update();
-            model.Draw();
+            if (shouldDraw)
+                model.Draw();
         }
 
         model.WriteAllModelInfo();

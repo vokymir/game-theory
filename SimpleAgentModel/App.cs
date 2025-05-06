7dc7716a25036ce087a9ecbd85eedbaad6294929
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-05-06 12:24:59 +0200

    simple running control

--- SimpleAgentModel/App.cs
+++ SimpleAgentModel/App.cs
@@ -8,14 +8,27 @@ public class App
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             AnsiHelper.Enable();
 
+        RunModel();
+    }
+
+    public static void RunModel()
+    {
+        bool wait = true;
         var model = new Model(50, 15, "./Agents/Forest.agent");
 
         model.Randomize();
         model.Draw();
+        string? inp;
 
         while (!model.ShouldEnd)
         {
-            Thread.Sleep(500);
+            if (wait)
+            {
+                inp = Console.ReadLine();
+                if (inp is not null && inp.StartsWith("c"))
+                    wait = false;
+            }
+
             model.Update();
             model.Draw();
         }
